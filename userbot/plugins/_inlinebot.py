5fe3244342cacdf8890a2c8354bec70c325844b9
StarkGang <StarkGangZ@gmail.com>
2020-09-27 22:54:05 +0530

    Update _inlinebot.py

--- userbot/plugins/_inlinebot.py
+++ userbot/plugins/_inlinebot.py
@@ -9,15 +9,15 @@ from telethon.tl import custom
 from userbot import CMD_LIST
 
 if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
-@tgbot.on(events.InlineQuery)  # pylint:disable=E0602
-async def inline_handler(event):
-    builder = event.builder
-    result = None
-    query = event.text
-    if event.query.user_id == bot.uid and query.startswith("Friday"):
-        rev_text = query[::-1]
-        buttons = paginate_help(0, CMD_LIST, "helpme")
-        result = builder.article(
+    @tgbot.on(events.InlineQuery)  # pylint:disable=E060
+    async def inline_handler(event):
+        builder = event.builder
+        result = None
+        query = event.text
+        if event.query.user_id == bot.uid and query.startswith("Friday"):
+            rev_text = query[::-1]
+            buttons = paginate_help(0, CMD_LIST, "helpme")
+            result = builder.article(
             "© Userbot Help",
             text="{}\nCurrently Loaded Pluins: {}".format(query, len(CMD_LIST)),
             buttons=buttons,
@@ -36,6 +36,7 @@ async def inline_handler(event):
     buttonz = builder.article(title="Stats Module", text=query, buttons=butto)
     await event.answer([buttonz])
 
+
     @tgbot.on(
         events.callbackquery.CallbackQuery(  # pylint:disable=E0602
             data=re.compile(b"helpme_next\((.+?)\)")
@@ -50,6 +51,7 @@ async def inline_handler(event):
         else:
             reply_popp_up_alert = "Please get your own Userbot, and don't use mine!"
             await event.answer(reply_popp_up_alert, cache_time=0, alert=True)
+
 
     @tgbot.on(
         events.callbackquery.CallbackQuery(  # pylint:disable=E0602
@@ -67,6 +69,7 @@ async def inline_handler(event):
         else:
             reply_pop_up_alert = "Please get your own Userbot, and don't use mine!"
             await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
+
 
     @tgbot.on(
         events.callbackquery.CallbackQuery(  # pylint:disable=E0602
@@ -121,15 +124,15 @@ def paginate_help(page_number, loaded_plugins, prefix):
     modulo_page = page_number % max_num_pages
     if len(pairs) > number_of_rows:
         pairs = pairs[
-            modulo_page * number_of_rows : number_of_rows * (modulo_page + 1)
-        ] + [
-            (
-                custom.Button.inline(
-                    "Previous", data="{}_prev({})".format(prefix, modulo_page)
-                ),
-                custom.Button.inline(
-                    "Next", data="{}_next({})".format(prefix, modulo_page)
-                ),
-            )
-        ]
+                modulo_page * number_of_rows: number_of_rows * (modulo_page + 1)
+                ] + [
+                    (
+                        custom.Button.inline(
+                            "Previous", data="{}_prev({})".format(prefix, modulo_page)
+                        ),
+                        custom.Button.inline(
+                            "Next", data="{}_next({})".format(prefix, modulo_page)
+                        ),
+                    )
+                ]
     return pairs
