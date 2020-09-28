4237b9d13960f675b244c0dd60ae3af57714c08f
StarkGang <StarkGangZ@gmail.com>
2020-09-28 19:07:13 +0530

    Update hack.py

--- userbot/plugins/hack.py
+++ userbot/plugins/hack.py
@@ -11,20 +11,22 @@ async def _(event):
     input_str = event.pattern_match.group(1)
     rip = await edit_or_reply(event, "`Searching For Users Data Folder 📂`"
     animation_chars = [
-            "`Data Found! Starting Hack.`",
-            "`Please Wait. Starting Termal....`",
-            "`Hacking... 0%\n▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ `",
-            "`Hacking... 4%\n█▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ `",
-            "`Hacking... 8%\n██▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ `",    
-            "`Hacking... 20%\n█████▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ `",
-            "`Hacking... 36%\n█████████▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ `",
-            "`Hacking... 52%\n█████████████▒▒▒▒▒▒▒▒▒▒▒▒ `",
-            "`Hacking... 84%\n█████████████████████▒▒▒▒ `",
-            "`Hacking... 100%\n███████████████████████`",
-            "`Hacking Sucessfull ! Now Please Wait ! Exporting Data...... `"
-            "`Exporting Data To userbot/plugins/hackdata`..."
-            "`Export Complete. Now Check Your Folder 📂`"
+    "`Data Found! Starting Hack.`",
+    "`Please Wait. Starting Termal....`",
+    "`Hacking... 0%\n▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ `",
+    "`Hacking... 4%\n█▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ `",
+    "`Hacking... 8%\n██▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ `",    
+    "`Hacking... 20%\n█████▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ `",
+    "`Hacking... 36%\n█████████▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ `",
+    "`Hacking... 52%\n█████████████▒▒▒▒▒▒▒▒▒▒▒▒ `",
+    "`Hacking... 84%\n█████████████████████▒▒▒▒ `",
+    "`Hacking... 100%\n███████████████████████`",
+    "`Hacking Sucessfull ! Now Please Wait ! Exporting Data...... `"
+    "`Exporting Data To userbot/plugins/hackdata`..."
+    "`Export Complete. Now Check Your Folder 📂`"
     ]
     for i in animation_ttl:
-         await asyncio.sleep(animation_interval)
-         await rip.edit(animation_chars[i % 11])
+                await asyncio.sleep(animation_interval)
+                await rip.edit(animation_chars[i % 11])
+    else:
+        await event.edit("No User is Defined\n are u dumb\n reply to a user.")
