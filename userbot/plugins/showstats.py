3469cfba1b0a5b5cb0a782a56fd822175895b269
StarkGang <StarkGangZ@gmail.com>
2020-09-28 20:32:08 +0530

    Update showstats.py

--- userbot/plugins/showstats.py
+++ userbot/plugins/showstats.py
@@ -16,3 +16,16 @@ async def stats(event):
     tap = await bot.inline_query(botusername, noob) 
     await tap[0].click(event.chat_id)
     await event.delete()
+
+
+@borg.on(admin_cmd(pattern="xogame$"))
+async def stats(event):
+    if event.fwd_from:
+        return
+    botusername = "@xobot"
+    noob = "play"
+    if event.reply_to_msg_id:
+        reply_to_id = await event.get_reply_message()
+    tap = await bot.inline_query(botusername, noob) 
+    await tap[0].click(event.chat_id)
+    await event.delete()
