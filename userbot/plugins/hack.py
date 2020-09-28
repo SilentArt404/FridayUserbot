9855cd283e21e943a70c16613df1f66616a61a28
StarkGang <StarkGangZ@gmail.com>
2020-09-28 19:25:18 +0530

    Update hack.py

--- userbot/plugins/hack.py
+++ userbot/plugins/hack.py
@@ -2,7 +2,7 @@ from telethon import events
 import asyncio
 from userbot.utils import admin_cmd, sudo_cmd, edit_or_reply
 
-@borg.on(admin_cmd(pattern=r"hack", outgoing=True))
+@borg.on(admin_cmd(pattern="hack"))
 @borg.on(sudo_cmd(pattern=r"hack", allow_sudo=True))
 async def _(event):
     if event.fwd_from:
